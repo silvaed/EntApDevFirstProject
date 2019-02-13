4f85f8ee9c62b1e5c91a7661016a36d47fa91403
silvaed <edsonbr27@gmail.com>
2019-02-13 07:42:42 +0000

    Last Exercises done.

---
+++ EntApDevFristProject/EntApDevFristProject/Pages/DataType.cshtml.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EntApDevFristProject.Pages
+{
+    public class DataTypeModel : PageModel
+    {
+        [BindProperty]
+        [DataType(DataType.CreditCard)]
+        public string CreditCard { get; set; }
+
+        [BindProperty]
+        [DataType(DataType.Currency)]
+        public string Currency { get; set; }
+
+        [BindProperty]
+        [DataType(DataType.Duration)]
+        public string Duration { get; set; }
+
+        [BindProperty]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [BindProperty]
+        [DataType(DataType.Html)]
+        public string Html { get; set; }
+        [BindProperty]
+        [DataType(DataType.ImageUrl)]
+        public string ImageUrl { get; set; }
+        [BindProperty]
+        [DataType(DataType.MultilineText)]
+        public string MlText { get; set; }
+        [BindProperty]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [BindProperty]
+        [DataType(DataType.PhoneNumber)]
+        public string PhoneNumber { get; set; }
+
+
+        [BindProperty]
+        [DataType(DataType.PostalCode)]
+        public string PostalCode { get; set; }
+
+
+        [BindProperty]
+        [DataType(DataType.Url)]
+        public string WebAddress { get; set; }
+
+        [BindProperty]
+        [DataType(DataType.Upload)]
+        public string Upload { get; set; }
+
+        public void OnGet()
+        {
+
+        }
+
+        public void OnPost()
+        {
+
+        }
+    }
+}
+
+/*namespace EntApDevFristProject.Pages
+{
+    public enum DataType
+    {
+
+        Custom = 0,
+        DateTime = 1,
+        Date = 2,
+        Time = 3,
+        Duration = 4,
+        PhoneNumber = 5,
+        Currency = 6,
+        Text = 7,
+        Html = 8,
+    }
+
+
+    public class DataTypeModel : PageModel
+    {
+        public string gabriel { get; set; }
+
+
+        public void OnGet()
+        {
+
+        }
+    }
+}*/
---
+++ EntApDevFristProject/EntApDevFristProject/Pages/Time.cshtml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+
+namespace EntApDevFristProject.Pages
+{
+    public class TimeModel : PageModel
+    {
+        [BindProperty]
+        public bool[] DaysChoice { get; set; } = new bool[7];
+        public string[] DayOfWeek { get; set; } = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
+        [BindProperty]
+        public string[] ChoiceMonths { get; set; } = { };
+        public List<SelectListItem> Months { get; set; } = new List<SelectListItem>
+        { new SelectListItem ("Sunday", "Sunday"),
+          new SelectListItem ("Monday", "Monday"),
+          new SelectListItem ("Tuesday", "Tuesday"),
+          new SelectListItem ("Wednesday", "Wednesday"),
+          new SelectListItem ("Thursday", "Thursday"),
+          new SelectListItem ("Friday", "Friday"),
+          new SelectListItem ("Satuday", "Saturday"),
+        };
+
+        //public List<SelectListItem> Months { get; set; } =
+        //new List<SelectListItem>
+        //{ new SelectListItem(){Text="Janeiro", Value="Janeiro"},
+          //  new SelectListItem(){Text="2", Value="2"}
+        //};
+
+        public void OnGet()
+        {
+
+        }
+
+        public void OnPost()
+        {
+            
+        }
+
+
+    }
+}
